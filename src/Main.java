b0857ed91f10869d75f3a615084f8ca171b7532b
Deepak Kharol <dkharol48@gmail.com>
2024-05-17 03:34:36 +0530

    First commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("first line of code");
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        int n = matrix.length;
+
+        // Print the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
